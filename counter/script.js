9449ba8f411adb403620bbc14165ca99a0b766cb
dielduarte <magdiel.kesller@gmail.com>
2016-08-09 22:40:25 -0300

    commit

--- counter/script.js
+++ counter/script.js
@@ -1,48 +1,16 @@
-// Code goes here
+import React from 'react';
 
-var Button = React.createClass({
-    localHandleClick: function() {
-        this.props.localHandleClick(this.props.increment);
-    },
+
+var DevInCompany = React.createClass({
     render: function() {
         return (
-            <button onClick={this.localHandleClick}> +{this.props.increment} </button>
-        )
-    }
-});
-
-
-var Result = React.createClass({
-    render: function() {
-        return (
-            <div>{this.props.localCounter}</div>
+            <h1>
+                { this.props.message }
+            </h1>
         )
     }
 });
 
 
 
-var Main = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-    handleClick: function(increment) {
-        this.setState({counter: this.state.counter+increment});
-    },
-    render: function() {
-        return (
-            <div>
-                <Button localHandleClick={this.handleClick} increment={1}/>
-                <Button localHandleClick={this.handleClick} increment={10}/>
-                <Button localHandleClick={this.handleClick} increment={50}/>
-                <Button localHandleClick={this.handleClick} increment={100}/>
-                <Result localCounter={this.state.counter}/>
-            </div>
-        )
-    }
-});
-
-
-React.render(<Main />, document.getElementById('root'));
+React.render(<DevInCompany  message="oi dev in company!" />, document.getElementById('root'));
